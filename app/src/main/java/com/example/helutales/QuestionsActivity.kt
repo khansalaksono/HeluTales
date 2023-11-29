0b5df5c5c99ba90b260496dd19126db7298d974e
khansalaksono <khansalaksono@gmail.com>
2023-11-30 02:46:34 +0700

    BERHASIL

--- app/src/main/java/com/example/helutales/QuestionsActivity.kt
+++ app/src/main/java/com/example/helutales/QuestionsActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,14 +15,18 @@ import com.google.gson.Gson
 
 class QuestionsActivity : AppCompatActivity() {
 
-    var quizzes: MutableList<Quiz>? = null
+    //var quizzes: MutableList<Quiz>? = null
+    private var quiz: Quiz? = null
     var questions: MutableMap<String, Question>? = null
     var index = 1
-
+    private lateinit var quizTitle: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+
+        quizTitle = intent.getStringExtra("Title") ?: ""
+
         setUpFirestore()
         setUpEventListener()
     }
@@ -41,31 +46,84 @@ class QuestionsActivity : AppCompatActivity() {
         }
 
         btnSubmit.setOnClickListener {
-            Log.d("FINALQUIZ", questions.toString())
+            if (quiz != null) {
+                Log.d("FINALQUIZ", questions.toString())
 
-            val intent = Intent(this, ResultActivity::class.java)
-            val json = Gson().toJson(quizzes!![0])
-            intent.putExtra("QUIZ", json)
-            startActivity(intent)
+                val intent = Intent(this, ResultActivity::class.java)
+                val json = Gson().toJson(quiz)
+                intent.putExtra("QUIZ", json)
+                startActivity(intent)
+            } else {
+                // Handle the case where quiz is null
+                Toast.makeText(this, "No quiz available", Toast.LENGTH_SHORT).show()
+            }
         }
+
+
+//        btnSubmit.setOnClickListener {
+//            if (quizzes != null && quizzes!!.isNotEmpty()) {
+//                Log.d("FINALQUIZ", questions.toString())
+//
+//                val intent = Intent(this, ResultActivity::class.java)
+//                val json = Gson().toJson(quizzes!![0])
+//                intent.putExtra("QUIZ", json)
+//                startActivity(intent)
+//            } else {
+//                // Handle the case where quizzes is null or empty
+//                Toast.makeText(this, "No quizzes available", Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 
     private fun setUpFirestore() {
         val firestore = FirebaseFirestore.getInstance()
-        val quizTitle = intent.getStringExtra("Title")
-        if (quizTitle != null) {
+
+        if (quizTitle.isNotEmpty()) {
             firestore.collection("quizzes").whereEqualTo("title", quizTitle)
                 .get()
                 .addOnSuccessListener { result ->
                     if (result != null && !result.isEmpty) {
                         val document = result.documents[0]
-                        val quiz = document.toObject(Quiz::class.java)
-                        questions = quiz?.questions?.toMutableMap()
-                        bindViews()
+                        quiz = document.toObject(Quiz::class.java)
+
+                        if (quiz != null) {
+                            questions = quiz?.questions?.toMutableMap()
+                            bindViews()
+                        } else {
+                            // Handle the case where the retrieved quiz is null
+                            Toast.makeText(this, "No quiz available", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        // Handle the case where no quiz is found
+                        Toast.makeText(this, "No quiz found", Toast.LENGTH_SHORT).show()
                     }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle the failure to retrieve data from Firestore
+                    Log.e("QuestionsActivity", "Error getting quiz questions", exception)
+                    Toast.makeText(this, "Error retrieving quiz", Toast.LENGTH_SHORT).show()
                 }
         }
     }
+
+
+
+//    private fun setUpFirestore() {
+//        val firestore = FirebaseFirestore.getInstance()
+//        val quizTitle = intent.getStringExtra("Title")
+//        if (quizTitle != null) {
+//            firestore.collection("quizzes").whereEqualTo("title", quizTitle)
+//                .get()
+//                .addOnSuccessListener { result ->
+//                    if (result != null && !result.isEmpty) {
+//                        val document = result.documents[0]
+//                        val quiz = document.toObject(Quiz::class.java)
+//                        questions = quiz?.questions?.toMutableMap()
+//                        bindViews()
+//                    }
+//                }
+//        }
+//    }
 
     private fun bindViews() {
         val btnPrevious = findViewById<Button>(R.id.btnPrevious)
@@ -94,5 +152,13 @@ class QuestionsActivity : AppCompatActivity() {
             optionList.adapter = optionAdapter
             optionList.setHasFixedSize(true)
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        // Pass the quiz title back to MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("Title", quizTitle)
+        startActivity(intent)
     }
 }
